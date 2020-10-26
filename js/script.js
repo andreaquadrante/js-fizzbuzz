877f29bf6bcdbbfa3bb651e573b82dc396946e67
Andrea <quadranteandrea@gmail.com>
2020-10-26 17:04:11 +0100

    Custom

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 var fizzBuzzListEl = document.getElementById('fizzBuzzList');
 
+// PIPPO stands for FIZZ, PLUTO stands for BUZZ
+
 for (var i = 1; i <= 100; i++) {
     if (i % 15 == 0) {
-      fizzBuzzListEl.innerHTML += '<li>' + 'FizzBuzz' + '</li>';
+      fizzBuzzListEl.innerHTML += '<li>' + 'PippoPluto' + '</li>';
     } else if (i % 3 == 0) {
-        fizzBuzzListEl.innerHTML += '<li>' + 'Fizz' + '</li>';
+        fizzBuzzListEl.innerHTML += '<li>' + 'Pippo' + '</li>';
     } else if (i % 5 == 0) {
-        fizzBuzzListEl.innerHTML += '<li>' + 'Buzz' + '</li>'
+        fizzBuzzListEl.innerHTML += '<li>' + 'Pluto' + '</li>'
     } else
         fizzBuzzListEl.innerHTML += '<li>' + i + '</li>'
 }
